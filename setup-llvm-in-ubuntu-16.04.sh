e6c46f7d39a82702bee3ed07be684ba5a8c0ed08
cool-pot <peterzhouyi@gmail.com>
2019-05-18 00:41:50 -0400

    add setup file for ubuntu

---
+++ setup-llvm-in-ubuntu-16.04.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install flex
+sudo apt-get install bison
+sudo apt-get install llvm
+sudo apt-get install clang
+sudo apt-get install zlib1g-dev
